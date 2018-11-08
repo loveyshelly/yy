22bb8e3db124e1b9c54fcfd6974f5d07b4841aa1
loveyshelly <loveyshelly@gmail.com>
2018-11-08 17:02:24 +0800

    优化登录页面

--- case/test_login_case.py
+++ case/test_login_case.py
@@ -3,6 +3,7 @@
 from page.login_page import LoginPage
 from selenium import webdriver
 import unittest
+from common.logger import logger as log
 
 class LoginCase(unittest.TestCase):
     @classmethod
@@ -11,21 +12,16 @@ class LoginCase(unittest.TestCase):
         cls.loginpage=LoginPage(cls.driver) #实例化登录页面
 
     def test_login(self):
-        # 登录
-        self.loginpage.login("13544772266","123456")
-        #判断是否有弹窗
-        # self.loginpage.is_alert_exsit()
-        # 获取登录结果
-        re=self.loginpage.get_login_result()
-        self.assertEqual(re,"13544772266")
-
-    def tearDown(self):
-        self.driver.delete_all_cookies()#清空cookies
-        self.driver.refresh()
+        username = "13544772266"
+        psw = "123456"
+        # 获取实际结果
+        re=self.loginpage.login(username,psw)
+        #期望结果
+        log.info("re")
 
     @classmethod
     def tearDownClass(cls):
-        cls.driver.quit()
+        pass
 
 if __name__=="__main__":
     unittest.main()
--- page/login_page.py
+++ page/login_page.py
@@ -1,13 +1,14 @@
 #-*- coding: UTF-8 -*-.
 from common.base import Base
+from common.logger import logger as log
 
 loginurl = "https://pc.test-chexiu.cn/login/login"
 
 class LoginPage(Base):
     '''登录页面'''
-    user_loc=("xpath","//*[@id='app']/div/div/login/div/div/div/div[2]/div/form/div[2]/div[1]/input")#输入账号
-    psw_loc=("xpath","//*[@id='app']/div/div/login/div/div/div/div[2]/div/form/div[2]/div[2]/input")#输入密码
-    sub_loc=("xpath","//*[@id='app']/div/div/login/div/div/div/div[2]/div/form/div[2]/button")#点击登录
+    user_loc=("xpath","//input[@placeholder='账号']")#输入账号
+    psw_loc=("xpath","//input[@type='password']")#输入密码
+    sub_loc=("xpath","//span[contains(text(),'登录')]")#点击登录
     zhanghao_loc=("xpath","//*[@id='app']/div/div/layout/div/headbar/header/div/div[2]/div/div[3]/dropdown/div/span/div/span")
 
     def open_login_page(self):
@@ -19,48 +20,14 @@ class LoginPage(Base):
         self.driver.delete_all_cookies() # 删除所有的cookies
         self.driver.refresh()
 
-    def input_username(self,usrname):
-        '''输入账号'''
-        self.sendKeys(self.user_loc, usrname)
 
-    def input_psw(self, psw):
-        '''输入密码'''
-        self.sendKeys(self.psw_loc, psw)
-
-    def click_login_button(self):
-        '''点击登录按钮'''
-        self.click(self.sub_loc)
-
-    def login(self, username="13544772266", psw="123456"):
+    def login(self, username, psw):
         '''登录流程:'''
+        log.info("begin login page")
         self.open_login_page()
-        self.input_username(username)
-        self.input_psw(psw)
-        self.click_login_button()
+        self.sendKeys(LoginPage.user_loc,username)#输入账号
+        self.sendKeys(LoginPage.psw_loc,psw)#输入密码
+        self.click(LoginPage.sub_loc)#点击登录按钮
+        log.info("end login page")
 
-    def get_login_result(self):
-        '''获取登录的结果'''
-        try:
-            t = self.findElement(self.zhanghao_loc).text
-            return t
-        except:
-               print("登录失败！！！，返回空字符")
-               return ""
 
-    def is_alert(self):
-        '''判断是否有弹窗，有的话点确定，没有的话就pass
-        有缺陷：如果页面卡，出现alert慢就判断不到了
-        '''
-        try:
-            alert = self.driver.switch_to_alert()  # 这一步不会报错
-            print(alert.text)     # 这一步，没有获取到alert文本就报错了
-            alert.accept()  # 点确定按钮
-        except:
-            pass
-
-    def is_alert_exsits_base(self):
-        try:
-            alert = self.is_alert_exsit()
-            alert.accept()
-        except:
-            pass
